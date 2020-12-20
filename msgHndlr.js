f1e6854a1e45d55b1802a5c1bc1765bb6f15e273
rashidsiregar28 <74960869+rashidsiregar28@users.noreply.github.com>
2020-12-21 00:04:24 +0700

    Add files via upload

--- msgHndlr.js
+++ msgHndlr.js
@@ -3785,11 +3785,13 @@ ${Object.keys(me.phone).map(key => `${key} : ${me.phone[key]}`).join('\n')}`.sli
                     } else {
                         const mentah = await aksa.checkNumberStatus(text) //VALIDATE WHATSAPP NUMBER
                         const hasill = mentah.canReceiveMessage ? `Sukses menambahkan nomer ke database\nTotal data nomer sekarang : *${pengirim.length}*` : false
-                        if (!hasill) return aksa.reply(dari, 'Nomor WhatsApp tidak valid [ Tidak terdaftar di WhatsApp ] atau gunakan 62 di awal bukan 0', id) {
+                        if (!hasill) return aksa.reply(dari, 'Nomor WhatsApp tidak valid [ Tidak terdaftar di WhatsApp ] atau gunakan 62 di awal bukan 0', id)
+                          {
                             pengirim.push(mentah.id._serialized)
                             fs.writeFileSync('./lib/user.json', JSON.stringify(pengirim))
                             aksa.sendText(dari, hasill)
-                    }}
+                    }
+                  }
                     break
                 case `${prefix}remove`: //menghapus nomor from database
                     if (!isOwner) return aksa.reply(dari, 'Fitur ini hanya dapat digunakan oleh Owner Lucya')
